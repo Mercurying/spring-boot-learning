fd3a0ef2cd2a945359ab1302fdde5367cdf30dd8
mercury <dr20131013@163.com>
2019-02-19 21:43:27 +0800

    添加spring actuator依赖包;

--- src/main/java/com/mercury/springbootlearning/SpringBootLearningApplication.java
+++ src/main/java/com/mercury/springbootlearning/SpringBootLearningApplication.java
@@ -3,11 +3,13 @@ package com.mercury.springbootlearning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 // @EnableJpaRepositories // 启用该注解添加自定义jpa实现类 默认无需添加@SpringBootApplication已配置
 // @EnableCaching 启用缓存配置
 @EnableCaching
+@EnableJpaRepositories(basePackages = "com.mercury.springbootlearning.dao")
 @EnableMongoRepositories(basePackages = "com.mercury.springbootlearning.repository")
 @SpringBootApplication
 public class SpringBootLearningApplication {
